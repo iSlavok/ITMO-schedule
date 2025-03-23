618a543251f0e3c09f6a0f2c3204bdecf99cf198
iSlavok <slavaterehin6@gmail.com>
2025-03-24 00:47:43 +0500

    fix db

--- app/database/connection.py
+++ app/database/connection.py
@@ -7,7 +7,11 @@ from sqlalchemy.orm import sessionmaker
 from app.database.base import Base
 
 load_dotenv()
-engine = create_engine(f"{os.getenv('DB_DRIVER')}:///{os.getenv('DB_NAME')}")
+engine = create_engine(f"{os.getenv('DB_DRIVER')}:///{os.getenv('DB_NAME')}",
+                       pool_size=20,  # Основной размер пула
+                       max_overflow=20,  # Максимальное количество дополнительных соединений
+                       pool_timeout=60,  # Время ожидания перед тайм-аутом
+                       )
 Session = sessionmaker(bind=engine)
 
 
